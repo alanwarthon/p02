3aaacba86fd82a34c35e8dd30d536fabd273566a
alanwarthon <familiawarthon@gmail.com>
2025-08-26 10:22:07 -0700

    template literall strings practice

--- script01.js
+++ script01.js
@@ -3,3 +3,14 @@ let num2 = Number(prompt("Pick another number "));
 
 let sum = num1 + num2;
 alert("The sum is:\n"+sum);
+
+/**
+ * Using a template literal string
+ */
+
+let amount_to_pay = 35;
+let item_name = 'Iphone 12 pro max';
+let client_name = 'Ethan';
+
+let thankyou_message = `Hi, ${client_name}, thanks for the order of the ${item_name}, the amount to pay is ${amount_to_pay}.00 CAD.`;
+console.log(thankyou_message);
